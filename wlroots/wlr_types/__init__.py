c7a361660a9e9420b05bcd6150588dabd937913b
RFCreate <107062289+RFCreate@users.noreply.github.com>
2026-08-04 18:45:10 -0600

    fix: wlr_input_inhibitor has been mark as deprecated

--- wlroots/wlr_types/__init__.py
+++ wlroots/wlr_types/__init__.py
@@ -16,7 +16,6 @@ from .foreign_toplevel_management_v1 import ForeignToplevelManagerV1  # noqa: F4
 from .fractional_scale_v1 import FractionalScaleManagerV1  # noqa: F401
 from .gamma_control_v1 import GammaControlManagerV1  # noqa: F401
 from .input_device import InputDevice  # noqa: F401
-from .input_inhibit import InputInhibitManager  # noqa: F401
 from .keyboard import Keyboard  # noqa: F401
 from .layer_shell_v1 import LayerShellV1  # noqa: F401
 from .matrix import Matrix  # noqa: F401
